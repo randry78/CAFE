381fdb0ab92e7cc89536a264abe096aff9ffe549
RAJAOFERSON DOminique Rina <rdr2510@gmail.com>
2023-08-17 00:25:52 -0400

    update

--- CAFE/src/Pages/Abouts.js
+++
@@ -1,8 +0,0 @@
-export default function Abouts(){
-    return(
-        <>
-            <p>A PROPOS</p>
-        </>
-    )
-}
-
--- CAFE/src/Pages/WishList.js
+++
@@ -1,7 +0,0 @@
-export default function WishList(){
-    return(
-        <>
-            <h1>LISTE SOUHAIT - COUP DE COEUR</h1>   
-        </>
-    )
-}
